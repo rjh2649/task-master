fc978984db91259cadbc7eacd88d7cd9c345ad36
Ryan Horn <rjh2649@gmail.com>
2024-06-29 14:39:23 -0500

    delete endpoint fully working as intended

--- app/src/main/java/com/nashss/se/taskmaster/lambda/DeleteTaskLambda.java
+++ app/src/main/java/com/nashss/se/taskmaster/lambda/DeleteTaskLambda.java
@@ -7,21 +7,16 @@ import com.nashss.se.taskmaster.activity.result.DeleteTaskResult;
 
 public class DeleteTaskLambda
         extends LambdaActivityRunner<DeleteTaskRequest, DeleteTaskResult>
-        implements RequestHandler<AuthenticatedLambdaRequest<DeleteTaskRequest>, LambdaResponse> { 
+        implements RequestHandler<LambdaRequest<DeleteTaskRequest>, LambdaResponse> { 
     @Override
-    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<DeleteTaskRequest> input, Context context) {
+    public LambdaResponse handleRequest(LambdaRequest<DeleteTaskRequest> input, Context context) {
         return super.runActivity(() -> {
-            DeleteTaskRequest unauthenticatedRequest = input.fromPath(path ->
-            DeleteTaskRequest.builder()
-                    .withId(path.get("taskId"))
-                    .build()
-            );
-            
-            return input.fromUserClaims(claims -> 
-                    DeleteTaskRequest.builder()
-                            .withUserId(claims.get("email"))
-                            .withId(unauthenticatedRequest.getId())
-                            .build());
+                return input.fromPath(path ->
+                        DeleteTaskRequest.builder()
+                        .withUserId(path.get("userId"))
+                        .withId(path.get("taskId"))
+                        .build()
+                );
         },
         (request, serviceComponent) -> 
                 serviceComponent.provideDeleteTaskActivity().handleRequest(request)
